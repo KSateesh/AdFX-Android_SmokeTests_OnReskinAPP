a24c7f67fc04f474e936a4f07e3c92361fd95356
KSateesh <sateesh.kumar.contractor@weather.com>
2015-11-23 16:40:38 +0530

    Completed Maps and News test cases
    Completed Maps and News test cases

--- com.twc.androidLogcat/src/test/java/com/twc/General/setAddress_Location.java
+++ com.twc.androidLogcat/src/test/java/com/twc/General/setAddress_Location.java
@@ -2,7 +2,6 @@ package com.twc.General;
 
 import io.appium.java_client.MobileElement;
 import io.appium.java_client.TouchAction;
-
 import org.openqa.selenium.Alert;
 import org.openqa.selenium.By;
 import org.openqa.selenium.Dimension;
@@ -19,133 +18,48 @@ import com.twc.driver.Driver;
 
 @SuppressWarnings("unused")
 public class setAddress_Location extends Driver {
-	
-	public void setLocation() throws InterruptedException{
-		
-		    //Units of Measure
-//        MobileElement ele = (MobileElement) Ad.findElementById("com.weather.Weather:id/temperature");
-//		System.out.println("Temperature : " + ele.getText());
-		
-	//	MobileElement ele1=(MobileElement) Ad.findElementById("com.weather.Weather:id/hurricane_central_module_title");
-		
-//		String ele1="Right Now";
-//		Ad.scrollToExact(ele1);
-		//System.out.println("Scrolling to Right now is done");
-		
-//		String ele2="10 Day";
-//		Ad.scrollToExact(ele2);	
-//		System.out.println("Scrolling to 10-Day is done");
-		
-//		String ele3="Maps";
-//		Ad.scrollToExact(ele3);
-//		System.out.println("Scrolling to Maps is done");
-		// Sign Up
-//		MobileElement el1 = (MobileElement) Ad.findElementById("com.weather.Weather:id/hourly_title_textview");
-//		System.out.println("Sign Up : " + el1.getText());
-		
-		// Moving from Units of Measure element to Sign Up element
-//		TouchAction action1 = new TouchAction(Ad);
-//		action1.longPress(ele).moveTo(el1).release().perform();
-		
-		
-//	
-//		Dimension dimensions = Ad.manage().window().getSize();
-//		System.out.println("dimensions :: "+dimensions);
-//		
-//		Double screenHeightStart = dimensions.getHeight() * 0.5;
-//		System.out.println("screenHeightStart :: "+screenHeightStart);
-//		
-//		int scrollStart = screenHeightStart.intValue();
-//		System.out.println("scroll to value = " +scrollStart);
-//		
-//		Double screenHeightEnd = dimensions.getHeight() * 0.1;  //  dimensions.getHeight() * 0.2;  == 512.0
-//		System.out.println("screenHeightEnd :: " +screenHeightEnd);
-//		
-//     	int scrollEnd = screenHeightEnd.intValue(); 
-//     	System.out.println("scroll to end = " +scrollEnd);
-//		int scrollStart =2300;
-//		int scrollEnd =20;
-//		Ad.swipe(0,scrollStart,0,scrollEnd,2000);
-//		Thread.sleep(10000);
+    
+    public void setLocation() throws InterruptedException{
+        
+        System.out.println("Setting Address on the APP");
+        
+        //Getting the present location on the APP
+        String presentLocation = Ad.findElement(By.id("com.weather.Weather:id/location_name")).getText();
+        
+        System.out.println("Present Location on App is :: "+presentLocation);
+        
+        Thread.sleep(2000);
+        
+        //Clicking on Search icon to enter the ZIP Code
+        
+        Ad.findElementByAccessibilityId("Search").click();
+        //	     Ad.findElementById("com.weather.Weather:id/search").click();
+        
+        System.out.println("Entering a ZipCode");
+        
+        Ad.findElementById("com.weather.Weather:id/search_src_text").sendKeys("10035");
+        //	     Ad.findElementByName("Address/City/Zip").sendKeys("10035");
+        
+        Thread.sleep(2000);
+        
+        //Selecting the Address by swipe method
+        Ad.swipe(280,380,1170,380,2000); //for s6
+        
+        //	     Ad.swipe(180,174,580,174,1000); //for nexus
+        
+        //	     Ad.swipe(180,290,690,290,1000); // for nexus-1
+        
+        System.out.println("Selected the Address");
+        
+        //Getting the updated location after selecting the entered ZipCode/Address
+        String updatedLocation = Ad.findElement(By.id("com.weather.Weather:id/location_name")).getText();
+        
+        System.out.println("Updated Location is :: " + updatedLocation);
+        
+        Thread.sleep(4000);
+        
+    }  
+    
+}
 
-
-		
-//		for (int i = 0; i < dimensions.getHeight(); i++) {
-//			System.out.println("Scrolling "+i+" time");
-//			Ad.swipe(0,scrollStart,0,scrollEnd,2000);
-//			Thread.sleep(6000);
-//			if (Ad.findElement(By.name("YourText")).size()>0)
-//			System.exit(i);
-			//}
-//			Ad.findElement(By.name("YourText")).click();
-
-//		System.out.println("Scroll done till all the pages");
-
-	
-		 String presentLocation = Ad.findElement(By.id("com.weather.Weather:id/location_name")).getText();
-
-	     System.out.println("Present Location is :: "+presentLocation);
-	     
-	     Thread.sleep(4000);
-	     	     
-	     Ad.findElementById("com.weather.Weather:id/search").click();
-
-	     Ad.findElement(By.id("android:id/search_src_text")).sendKeys("10035");
-	     
-	     Thread.sleep(4000);
-	     
-	     Ad.swipe(180,174,580,174,1000); //for nexus
-	     
-//	     Ad.swipe(280,380,1170,380,2000); //for s6
-	     
-//	     Ad.swipe(180,290,690,290,1000); // for nexus-1
-    	 
-//	     Thread.sleep(4000);
- 
-//	    WebElement setAddress = Ad.findElement(By.id("android:id/search_src_text"));
-//	    setAddress.sendKeys("10035");
-//	    Thread.sleep(4000);
-//	    setAddress.sendKeys(Keys.ARROW_DOWN);
-//	    setAddress.sendKeys(Keys.ENTER);
-	    
-//	    Ad.swipe(280,358,1170,358,2000);
-//	    Thread.sleep(4000);
-	    
-//	  Point y=  x.getLocation();
-//	  System.out.println(y);
-//	    x.sendKeys("10035");
-//	    x.sendKeys(Keys.ARROW_DOWN);
-//	    x.sendKeys(Keys.ENTER);
-
-//	     Thread.sleep(4000);
-//	     Actions actions=new Actions(Ad);
-//	     actions.sendKeys(Keys.ARROW_DOWN).click().build().perform();     
-//	     actions.sendKeys(Keys.ARROW_DOWN).click();
-//	     actions.sendKeys(Keys.ENTER);
-//	     actions.keyDown(Keys.ARROW_DOWN).click().build().perform();
-//
-//	     WebDriverWait wait= new WebDriverWait(Ad,10);
-//	     
-//	     wait.until(ExpectedConditions.presenceOfElementLocated());
-	     
-//	     Thread.sleep(4000);
-//	     
-//	     Select city = new Select(Ad.findElement(By.id("com.weather.Weather:id/search")));
-//	     city.selectByVisibleText("");
-//	     
-//	     try{
-//	     Alert al= Ad.switchTo().alert();
-//	    		 al.accept();
-//	    		 System.out.println("Alert present and handled it");
-//	     }catch (Exception e){
-//	    	 System.out.println("Exception message : "+e);
-//	    	 System.out.println("Alert is not presented");
-//	    	 }
-	}  
-	
-
-
-
-	}
-	
 
--- com.twc.androidLogcat/src/test/java/com/twc/General/toKnowBuildVersion.java
+++ com.twc.androidLogcat/src/test/java/com/twc/General/toKnowBuildVersion.java
@@ -16,159 +16,80 @@ import io.appium.java_client.MultiTouchAction;
 import io.appium.java_client.TouchAction;
 
 public class toKnowBuildVersion extends Driver {
-
-	public void moreOptionsClick() throws InterruptedException {
-
-		System.out.println("Get the Build version of APP");
-		
-		// Clicking on More Options
-		WebElement more = Ad.findElement(By.xpath("//android.view.View[1]/android.widget.FrameLayout[2]/android.view.View[1]/android.widget.LinearLayout[3]/android.widget.ImageButton[1]"));
-
-		more.click();
-
-	    // Clicking on Setting link
-		Ad.findElement(By.xpath("//android.widget.ListView[1]/android.widget.LinearLayout[2]/android.widget.RelativeLayout[1]/android.widget.TextView[1]")).click();
-
-		System.out.println("Searching for About this app element");
-		
-		WebElement aboutThisAPP=null;
-			
-			 try{
-				
-				 aboutThisAPP = Ad.findElementByName("About this app");
-				
-			    } catch(Exception e){ 
-					System.out.println("About this app element not found,so scroll down now");
-				}
-				
-				if(aboutThisAPP!=null && aboutThisAPP.isDisplayed())
-				{
-					System.out.println("About this app element is found, get build version");	
-					
-				//clicking on About this App
-					aboutThisAPP.click();
-				
-				//Getting the text of build version
-				String BuildVersion = Ad.findElement(By.id("com.weather.Weather:id/about_version")).getText();
-
-				System.out.println("Build Version is : " + BuildVersion);
-
-				// Clicking back button
-				Ad.findElement(By.id("android:id/home")).click();
-
-				// Clicking back button
-				Ad.findElement(By.id("android:id/home")).click();
-				} 
-				else{
-//					System.out.println("About this app element not found,so scroll down now");
-					
-					//Follow Me
-					MobileElement FollowMe = (MobileElement) Ad.findElementByName("Follow Me");
-					
-					// My Alerts
-					MobileElement MyAlerts = (MobileElement) Ad.findElementByName("My Alerts");
-					
-					// SCROLLING from My Alerts element to Follow Me element
-					TouchAction action1 = new TouchAction(Ad);
-					action1.longPress(FollowMe).moveTo(MyAlerts).release().perform();
-
-					WebDriverWait wait = new WebDriverWait(Ad, 10);
-					wait.until(ExpectedConditions.presenceOfElementLocated(By.name("About this app")));
-
-					Thread.sleep(2000);
-					
-					// clicking on About this App
-					Ad.findElementByName("About this app").click();
-					
-					// Getting the text of build version
-					String BuildVersion = Ad.findElement(By.id("com.weather.Weather:id/about_version")).getText();
-
-					System.out.println("Build Version is : " + BuildVersion);
-
-					// Clicking back button
-					Ad.findElement(By.id("android:id/home")).click();
-
-					// Clicking back button
-					Ad.findElement(By.id("android:id/home")).click();
-				}
-				
-				System.out.println("Build version taken from the About this app section under settings");
-				
-		
-		/*
-		WebElement aboutThisAPP=null;
-		try{
-//		 aboutThisAPP = Ad.findElement(By.xpath("//android.view.View[1]/android.widget.FrameLayout[2]/android.widget.LinearLayout[1]/android.widget.ListView[1]/android.widget.LinearLayout[7]/android.widget.RelativeLayout[1]/android.widget.TextView[1]"));
-		aboutThisAPP = Ad.findElement(By.xpath("//android.view.View[1]/android.widget.FrameLayout[2]/android.widget.LinearLayout[1]/android.widget.ListView[1]/android.widget.LinearLayout[8]/android.widget.RelativeLayout[1]/android.widget.TextView[1]"));
-		} catch(Exception e){ }
-		
-		if(aboutThisAPP!=null && aboutThisAPP.isDisplayed())
-		{
-		
-			System.out.println("About this app element is found, get build version");
-			
-		//clicking on About this App
-		Ad.findElement(By.xpath("//android.view.View[1]/android.widget.FrameLayout[2]/android.widget.LinearLayout[1]/android.widget.ListView[1]/android.widget.LinearLayout[8]/android.widget.RelativeLayout[1]/android.widget.TextView[1]")).click();
-		
-		//Getting the text of build version
-		String BuildVersion = Ad.findElement(By.id("com.weather.Weather:id/about_version")).getText();
-
-		System.out.println("Build Version is : " + BuildVersion);
-
-		// Clicking back button
-		Ad.findElement(By.id("android:id/home")).click();
-
-		// Clicking back button
-		Ad.findElement(By.id("android:id/home")).click();
-		} 
-		else{
-			System.out.println("About this app element not found,so scroll down now");
-			
-			// Units of Measure
-			MobileElement ele = (MobileElement) Ad.findElementByXPath("//android.view.View[1]/android.widget.FrameLayout[2]/android.widget.LinearLayout[1]/android.widget.ListView[1]/android.widget.LinearLayout[5]");
-			//System.out.println("Units of Measure : " + ele.getText());
-
-			// Sign Up
-			MobileElement el1 = (MobileElement) Ad.findElementByXPath("//android.view.View[1]/android.widget.FrameLayout[2]/android.widget.LinearLayout[1]/android.widget.ListView[1]/android.widget.LinearLayout[2]");
-			//System.out.println("Sign Up : " + el1.getText());
-			
-			// My Alerts
-			MobileElement MyAlerts = (MobileElement) Ad.findElementByXPath("//android.view.View[1]/android.widget.FrameLayout[2]/android.widget.LinearLayout[1]/android.widget.ListView[1]/android.widget.LinearLayout[2]");
-	    	//System.out.println("Sign Up : " + el1.getText());
-						
-			
-			// Moving from Units of Measure element to Sign Up element
-			TouchAction action1 = new TouchAction(Ad);
-			action1.longPress(ele).moveTo(el1).release().perform();
-
-			WebDriverWait wait = new WebDriverWait(Ad, 10);
-			wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//android.view.View[1]/android.widget.FrameLayout[2]/android.widget.LinearLayout[1]/android.widget.ListView[1]/android.widget.LinearLayout[7]/android.widget.RelativeLayout[1]/android.widget.TextView[1]")));
-
-			Thread.sleep(2000);
-			// clicking on About this App
-			Ad.findElement(By.xpath("//android.view.View[1]/android.widget.FrameLayout[2]/android.widget.LinearLayout[1]/android.widget.ListView[1]/android.widget.LinearLayout[8]/android.widget.RelativeLayout[1]/android.widget.TextView[1]")).click();
-			
-			// Getting the text of build version
-			String BuildVersion = Ad.findElement(By.id("com.weather.Weather:id/about_version")).getText();
-
-			System.out.println("Build Version is : " + BuildVersion);
-
-			// Clicking back button
-			Ad.findElement(By.id("android:id/home")).click();
-
-			// Clicking back button
-			Ad.findElement(By.id("android:id/home")).click();
-		}
-		
-		System.out.println("build version taken from the About this app section under settings");
-
-		// About multi touch actions
-		// MultiTouchAction multiTouch = new MultiTouchAction(Ad);
-		// TouchAction action0 = new TouchAction(Ad).tap(el);
-		// TouchAction action1 = new TouchAction(driver).tap(el);
-		// TouchAction action2 = new TouchAction(driver).tap(el);
-		// multiTouch.add(action0).add(action1).add(action2).perform(); */
-
-	}
-
+    
+    WebElement aboutThisAPP = null;
+    WebElement backButton = null;
+    
+    public void moreOptionsClick() throws InterruptedException {
+        
+        System.out.println("Get the Build version of APP");
+        
+        //Wait for 20 sec for element presence
+        WebDriverWait wait = new WebDriverWait(Ad, 6);
+        wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//android.widget.LinearLayout[1]/android.widget.FrameLayout[1]/android.widget.LinearLayout[1]/android.widget.FrameLayout[1]/android.support.v4.widget.DrawerLayout[1]/android.widget.RelativeLayout[1]/android.view.View[1]/android.widget.ImageButton[1]")));
+        
+        // Clicking on Menu Options
+        WebElement menu = Ad.findElement(By.xpath("//android.widget.LinearLayout[1]/android.widget.FrameLayout[1]/android.widget.LinearLayout[1]/android.widget.FrameLayout[1]/android.support.v4.widget.DrawerLayout[1]/android.widget.RelativeLayout[1]/android.view.View[1]/android.widget.ImageButton[1]"));
+        
+        menu.click();
+        
+        // Clicking on Setting link
+        
+        Ad.findElementByName("Settings").click();
+        
+        // System.out.println("Searching for About this app element");
+        
+        
+        System.out.println("About this app element is found, click on the same");
+        
+        aboutThisAPP = Ad.findElementByName("About this app");
+        
+        aboutThisAPP.click();
+        
+        String BuildVersion = Ad.findElementByName("English (°F/MPH/IN)").getText();
+        
+        // String BuildVersion = Ad.findElementByXPath("//android.widget.LinearLayout[1]/android.widget.FrameLayout[1]/android.widget.LinearLayout[1]/android.widget.FrameLayout[1]/android.widget.RelativeLayout[1]/android.widget.FrameLayout[1]/android.widget.LinearLayout[1]/android.widget.ListView[1]/android.widget.LinearLayout[2]/android.widget.RelativeLayout[1]/android.widget.TextView[2]").getText();
+        
+        System.out.println("Build Version is : " + BuildVersion);
+        
+        backButton = Ad.findElementByXPath("//android.widget.LinearLayout[1]/android.widget.FrameLayout[1]/android.widget.LinearLayout[1]/android.widget.FrameLayout[1]/android.widget.RelativeLayout[1]/android.widget.LinearLayout[1]/android.view.View[1]/android.widget.ImageButton[1]");
+        
+        backButton.click();
+        
+        backButton.click();
+        
+        //		try {
+        //
+        //			aboutThisAPP = Ad.findElementByName("About this app");
+        //
+        //		} catch (Exception e) {
+        //			// System.out.println("About this app element not found,so scroll down now");
+        //		}
+        //
+        //		if (aboutThisAPP != null && aboutThisAPP.isDisplayed()) {
+        //			System.out.println("About this app element is found, get build version");
+        //
+        //			// clicking on About this App
+        //			aboutThisAPP.click();
+        //
+        //			// Getting the text of build version
+        //			String BuildVersion = Ad.findElementByName("English (°F/MPH/IN)").getText();
+        //
+        //			// String BuildVersion = Ad.findElementByXPath("//android.widget.LinearLayout[1]/android.widget.FrameLayout[1]/android.widget.LinearLayout[1]/android.widget.FrameLayout[1]/android.widget.RelativeLayout[1]/android.widget.FrameLayout[1]/android.widget.LinearLayout[1]/android.widget.ListView[1]/android.widget.LinearLayout[2]/android.widget.RelativeLayout[1]/android.widget.TextView[2]").getText();
+        //
+        //			System.out.println("Build Version is : " + BuildVersion);
+        //
+        //			// Clicking back button
+        //			backButton = Ad.findElementByXPath("//android.widget.LinearLayout[1]/android.widget.FrameLayout[1]/android.widget.LinearLayout[1]/android.widget.FrameLayout[1]/android.widget.RelativeLayout[1]/android.widget.LinearLayout[1]/android.view.View[1]/android.widget.ImageButton[1]");
+        //
+        //			backButton.click();
+        //
+        //			backButton.click();
+        //
+        //		}
+        
+        System.out.println("Build version taken from the About this app section under settings");
+        
+    }
+    
 }
